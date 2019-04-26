ac35bb0d930042eee471fa6a6c3f6e0d307640b8
farid2509 <farid.dadoboev@gmail.com>
2019-04-26 18:30:13 +0500

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class HashItems
+    {
+        public object Key { get; private set; }
+        public object Value { get; private set; }
+
+        public HashItems(object key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+
+    public class HashTable
+    {
+        private HashItems[] table;
+        private int _maxSize;
+
+        public HashTable(int size)
+        {
+            table = new HashItems[size];
+            _maxSize = size;
+        }
+
+        static void Main()
+        {
+
+        }
+
+        public void PutPair(object key, object value)
+        {
+            if (key == null || value == null)
+            {
+                throw new ArgumentNullException();
+            }
+            var index = Math.Abs(key.GetHashCode() % _maxSize);
+
+            while (true)
+            {
+                if (table[index] == null)
+                {
+                    break;
+                }
+                if (table[index].Key.Equals(key))
+                {
+                    break;
+                }
+                index = (index + 1) % _maxSize;
+            }
+            table[index] = new HashItems(key, value);
+        }
+
+        public object GetValueByKey(object key)
+        {
+            var index = Math.Abs(key.GetHashCode() % _maxSize);
+            var startindex = index;
+            while (true)
+            {
+                if (table[index].Key.Equals(key))
+                {
+                    return table[index].Value;
+                }
+                if (table[index] == null)
+                {
+                    return null;
+                }
+                if (startindex == index)
+                {
+                    return null;
+                }
+                index = (index + 1) % _maxSize;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HashTable;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            var check = new HashTable.HashTable(3);
+            for (var i = 0; i < 3; i++)
+            {
+                check.PutPair(i, i + 1);
+                Assert.AreEqual(i + 1, check.GetValueByKey(i));
+            }
+        }
+
+        [TestMethod]
+        public void SameKeyTest()
+        {
+            var check = new HashTable.HashTable(3);
+            check.PutPair(1, 1);
+            check.PutPair(1, 2);
+            Assert.AreEqual(2, check.GetValueByKey(1));
+        }
+
+        [TestMethod]
+        public void OneInTenThousandsTest()
+        {
+            var dict = new HashTable.HashTable(10000);
+            for (var i = 0; i < 10000; i++)
+            {
+                dict.PutPair(i, i);
+            }
+            Assert.AreEqual(42, dict.GetValueByKey(42));
+        }
+
+        [TestMethod]
+        public void ThousandInTenThousandsTest()
+        {
+            var dict = new HashTable.HashTable(10000);
+            for (var i = 1001; i < 11001; i++)
+            {
+                dict.PutPair(i, i);
+            }
+            for (var i = 0; i < 1000; i++)
+            {
+                Assert.IsNull(dict.GetValueByKey(i));
+            }
+        }
+    }
+}
